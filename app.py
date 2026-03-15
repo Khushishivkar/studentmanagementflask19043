bafe05e85b7aef084e042d4b1a150184e6040cde
Khushishivkar <shivkarkhushi4@gmail.com>
2026-03-15 21:34:38 +0530

    Change Flask app port to 10000
    Updated the port number for running the Flask app.

--- app.py
+++ app.py
@@ -38,6 +38,4 @@ def init_db():
 
 # ---------------- RUN APP ---------------
 if __name__ == "__main__":
-    init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
